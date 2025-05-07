1ed0e55e5f29c6a59dca4950be4c5a2cc6864894
Tyler M <tmccoid@unomaha.edu>
2025-05-07 15:26:26 -0500

    Changed Home page and added template page for each store to use

--- base/views.py
+++ base/views.py
@@ -1,12 +1,16 @@
 
-from django.shortcuts import render 
-from reviews.models import ReviewsAddress, ReviewsStore 
+from django.shortcuts import render, get_object_or_404
+from reviews.models import ReviewsAddress, ReviewsStore, ReviewsReview
   
 def home(request): 
     storeData = ReviewsStore.objects.all()
-    addressData = ReviewsAddress.objects.get(address_id=1)  # or any query you want
+    addressData = ReviewsAddress.objects.all()  # or any query you want
     return render(request, 'home.html', {'addressData': addressData, 'storeData': storeData})
   
-def review(request): 
-    return render(request, "review.html") 
+def store_detail(request, pk):
+    store = get_object_or_404(ReviewsStore, pk=pk)
+    reviews = ReviewsReview.objects.filter(store=store)
+    return render(request, 'store_detail.html', {'store': store, 'reviews': reviews})
+
+
 
