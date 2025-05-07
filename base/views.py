1956fa5f50e6fad04d645bbb8fe3d0634ca84899
Tyler M <tmccoid@unomaha.edu>
2025-05-07 13:39:08 -0500

    Testing making a list

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from reviews.models import ReviewsAddress, ReviewsStore 
   
 def home(request): 
-    storeData = ReviewsStore.objects.get(store_id=1)
+    storeData = ReviewsStore.objects.all()
     addressData = ReviewsAddress.objects.get(address_id=1)  # or any query you want
     return render(request, 'home.html', {'addressData': addressData, 'storeData': storeData})
   
